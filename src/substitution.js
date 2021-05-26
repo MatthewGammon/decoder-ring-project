a62dff16afee7aeb7682cc5f865d4587db4e1f0f
Matthew Gammon <mgammon91@gmail.com>
2021-05-26 14:26:50 -0400

    wrote out the substitution function

--- src/substitution.js
+++ src/substitution.js
@@ -5,11 +5,41 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-
+  const ogAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if(!alphabet) return false;
+    
+    const smolInput = input.toLowerCase();
+    const betaBet = alphabet.toLowerCase();
+    const betSet = new Set(betaBet);
+    
+    if (betaBet.length !== 26) return false;
+    if (betSet.size !== 26) return false;
+    let result = "";
+    if (encode){
+    for(let char of smolInput){
+      const index = ogAlphabet.indexOf(char);
+      if(index < 0){
+        result += char;
+      } else {
+        result += betaBet[index];
+      }
+    }
+  } else {
+    for(let char of smolInput){
+      const index = betaBet.indexOf(char);
+      if(index < 0){
+        result += char;
+      } else {
+        result += ogAlphabet[index];
+      }
+    }
   }
-
+    return result;
+  }
+  
+  
   return {
     substitution,
   };
