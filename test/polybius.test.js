fd03ce3560b8621bcfed5ae6fef087dec25113c9
Matthew Gammon <mgammon91@gmail.com>
2021-05-27 16:38:45 -0400

    wrote the first test for the polybius function

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,13 @@
 const expect = require("chai").expect
+const polybiusModule = require("../src/polybius");
+
+describe("polybius", () => {
+    it("should return false if input has an uneven number of characters when decoding", () => {
+        const input = "this is a test!!";
+
+        const expected = false;
+        const actual = polybiusModule.polybius(input, false);
+        expect(actual).to.equal(expected);
+    })
+    it("")
+})
