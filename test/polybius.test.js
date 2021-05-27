ecd64a272635be3ef398406cdfce2a0ce56faeda
Matthew Gammon <mgammon91@gmail.com>
2021-05-27 17:37:16 -0400

    wrote additonal tests for added coverage and to meet project requirements

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -9,5 +9,39 @@ describe("polybius", () => {
         const actual = polybiusModule.polybius(input, false);
         expect(actual).to.equal(expected);
     })
-    it("")
-})
+    it("should return a string when encoding", () => {
+        const input = "this is a test";
+
+        const expected = "string";
+        const actual = typeof polybiusModule.polybius(input);
+        expect(actual).to.equal(expected);
+    })
+    it("should maintain spaces throughout the encoding/decoding", () => {
+    const input = "this is a test";
+
+    const expected = "44324234 4234 11 44513444";
+    const actual = polybiusModule.polybius(input);
+    expect(actual).to.equal(expected);
+    })
+    it("should ignore capital letters", () => {
+        const input = "THIS IS A TEST";
+
+        const expected = "44324234 4234 11 44513444";
+        const actual = polybiusModule.polybius(input);
+        expect(actual).to.equal(expected);
+    })
+    it("should convert both 'i' and 'j' to 42 when encoding", () => {
+        const input = "this is ja test";
+
+        const expected = "44324234 4234 4211 44513444";
+        const actual = polybiusModule.polybius(input);
+        expect(actual).to.equal(expected);
+    })
+    it("should show both 'i' and 'j' when decoding a message", () => {
+        const input = "44324234 4234 4211 44513444";
+
+        const expected = "th(i/j)s (i/j)s (i/j)a test";
+        const actual = polybiusModule.polybius(input, false);
+        expect(actual).to.equal(expected);
+    })
+});
