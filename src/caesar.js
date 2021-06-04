6f10423c7299692d186e6d95fcba1fcf09ea92a2
Matthew Gammon <mgammon91@gmail.com>
2021-06-04 04:12:13 -0400

    added comments to code

--- src/caesar.js
+++ src/caesar.js
@@ -4,10 +4,12 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+
 
   function caesar(input, shift = 0, encode = true) {
+    //if an invalid shift length is entered, it will return false. 
     if (shift === 0 || shift < -25 || shift > 25) return false;
+    //if the shift argument is negative (characters will be shifted to the left) and encode arg is true, run the following...
     if (shift < 0 && encode){
       shift = 26 + (shift % 26);
     }
@@ -25,8 +27,9 @@ const caesarModule = (function () {
       } else {
         characterCode = characterCode;
       }
-      //takes the result string and uses the String.fromCharCode method, passed in our character code from the previous line, to extract a string value that will have been shifted from the input, and adds this value to the result string.
+      //takes the result string and uses the String.fromCharCode method, passed in our character code, to extract a string value that will have been shifted from the input, and adds this value to the result string.
       result += String.fromCharCode(characterCode);
+      //if encode is set to false, this code will run instead. 
     } else {
       if(characterCode >= 97 && characterCode <= 122){
         characterCode = (characterCode -97 - shift) % 26 + 97;
